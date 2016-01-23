67eb6a223698134a8e5e81bd9fca016f5ca0e9f3
andrewmarmion <andrew.marmion@gmail.com>
2016-01-23 12:23:48 -0800

    feat: user location found
    feat: initial page setup

--- Decidr/Decidr/ViewController.swift
+++ Decidr/Decidr/ViewController.swift
@@ -7,19 +7,37 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    
+    let locationManager = CLLocationManager()
+    var currentLocation = CLLocation()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func getVenue(sender: AnyObject) {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
 
 
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        currentLocation = manager.location!
+        print("Update")
+        print(currentLocation.coordinate.latitude)
+        print(currentLocation.coordinate.longitude)
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("Error while updating location " + error.localizedDescription)
+    }
 }
 
